8cb81e509a46e47763083117af965a06dc3fbfe3
Steven Roberts <sr320@uw.edu>
2022-11-10 10:13:27 -0800

    histogram of scale length

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -13,3 +13,9 @@ install.packages("ggplot2")
 library(ggplot2)
 
 ggplot(data = fish_data_cat, mapping = aes(x = length, y = scalelength, color = lakeid))
+
+#Plot histogram of scale length by fish categorical size
+ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
+  geom_histogram()
+
+
