71bb90b28faa9117128042ca4ee84f31b3bd399a
DillLittle <dfournie@uw.edu>
2022-11-11 13:14:31 -0800

    changed data name to accomodate new data sets

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -1,10 +1,11 @@
-fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
+fish_data <- read.csv("data/Gaeta_etal_CLC_data_1.csv")
 
 install.packages(dplyr)
 library(dplyr)
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
 
+#this code categorizes data by lengths greater than 300
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 
